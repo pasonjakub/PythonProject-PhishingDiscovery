54acf69d7f0194e07935e170ed835401d87b9325
WiesQ <45373270+WiesQ@users.noreply.github.com>
2021-11-16 19:38:32 +0100

    Logistic Regression
    Model and accuracy

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ df.head()
 
 # print(df.dtypes)
 
+
 df['Result'].unique()
 
 df_result = df[df['Result']==1]       # Legitimate
@@ -24,3 +25,18 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
 X_train_scaled = scale(X_train)
 X_test_scaled = scale(X_test)
 
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import GridSearchCV
+
+#Logistic Regression Grid Search
+# param_grid_lr = [{'penalty' : ['l1', 'l2'],'C' : np.logspace(-4, 4, 20),'solver' : ['liblinear']}]
+# optimal_param_lr = GridSearchCV(LogisticRegression(), param_grid_lr, cv=5, refit=True, verbose=3)
+# optimal_param_lr.fit(X_train_scaled, y_train)
+# optimal_param_lr.best_params_
+
+#Logistic Regression model training
+model = LogisticRegression( penalty='l1', C = 0.08858667904100823, random_state=0, solver='liblinear')
+model.fit(X_train_scaled, y_train)
+p_pred = model.predict_proba(X_test_scaled)
+y_pred = model.predict(X_test_scaled)
+print(model.score(X_test_scaled,y_test))
